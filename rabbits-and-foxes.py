daa58904b073d20e6cc35e320df768e7f3ed8334
Madison Janvrin <janvrin.m@husky.neu.edu>
2016-10-19 12:45:38 -0400

    Improved speed some (~12s faster/100 trials)

--- rabbits-and-foxes.py
+++ rabbits-and-foxes.py
@@ -1,5 +1,4 @@
 import numpy as np
-from matplotlib import pyplot as plt
 import random
 
 k1 = 0.015
@@ -8,12 +7,10 @@ k3 = 0.0004
 k4 = 0.04
 
 '''Peak selection adopted from Greg Zaylor's notebook - thank you Greg'''
-#create counter for number of times foxes/rabbits die out:
-#zeropopulation=0
 #create list to store values at peaks for foxes and times:
 foxes = [[0,0],[0,0]]
 #define number of iterations
-Trials = 30
+Trials = 100
 
 def experiment(Trials):
     global zeropopulation
@@ -27,8 +24,8 @@ def experiment(Trials):
         R = R_0
         F = F_0
         time = 0
-        solution_exp = np.zeros((1,3))
-        solution_exp[0] = 0, R_0, F_0
+        solution_exp = np.zeros((1,2))
+        solution_exp[0] = 0, F_0
         while time < end:
             Rb = k1*R
             Rd = k2*R*F
@@ -46,33 +43,23 @@ def experiment(Trials):
             else:
                 F = F - 1
 
-            delta_t = 1/Rcum * np.log(1/u)
-            time_new = time + delta_t
-            solution_exp = np.append(solution_exp,[[time_new,R,F]],axis=0)
+            time = time + 1/Rcum * np.log(1/u)
+            solution_exp = np.append(solution_exp,[[time,F]],axis=0)
 
             if F==0:
+                zeropopulation = zeropopulation + 1
                 break
 
-            time = time_new
-        #plot all values on the same graph
         time_exp = solution_exp[:,0]
-        R_exp = solution_exp[:,1]
-        F_exp = solution_exp[:,2]
 
-        if i<20:
-            plt.plot(time_exp,R_exp,'b')
-            plt.plot(time_exp,F_exp,'g-')
-
-        filtered = F_exp * (time_exp>200) * (F_exp>200)
+        filtered = solution_exp[:,1] * (time_exp>200) * (solution_exp[:,1]>200)
         c = filtered.max()
         t = time_exp[filtered.argmax()]
         #if 2nd peak exists, add number of foxes and time it occurs to storage list. only return first peak if peak repeats itself
         if c>200:
             foxes[1].append(c)
             foxes[0].append(t)
-        #if 2nd peak doesn't exist, add to the counter for number of times foxes die
-        else:
-            zeropopulation = zeropopulation + 1
+
     return foxes
 
 experiment(Trials)
